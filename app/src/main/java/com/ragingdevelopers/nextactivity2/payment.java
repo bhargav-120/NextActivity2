ff0000be482a8a1172b4f4db538eb7c6f80a60a3
bhargavandroid <bhargavpatel2809@gmail.com>
2020-02-25 15:25:53 +0530

    final by bhargav

--- app/src/main/java/com/ragingdevelopers/nextactivity2/payment.java
+++ app/src/main/java/com/ragingdevelopers/nextactivity2/payment.java
@@ -2,13 +2,27 @@ package com.ragingdevelopers.nextactivity2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class payment extends AppCompatActivity {
+    private Button course1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+        course1 = findViewById(R.id.button4);
+
+        course1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(payment.this,Courses.class);
+                startActivity(intent);
+            }
+        });
     }
 }
